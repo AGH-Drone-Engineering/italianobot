07287c6bbcfa335a83a4c7b730d3cdaf0b902f59
Carbon225 <carbon225@protonmail.com>
2024-02-17 22:05:22 +0100

    aruco detector

--- itabot_aruco/itabot_aruco/detector.py
+++ itabot_aruco/itabot_aruco/detector.py
@@ -1,39 +1,48 @@
 import rclpy
 from rclpy.node import Node
+from sensor_msgs.msg import CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+import numpy as np
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 
-from std_msgs.msg import String
-
-
-class MinimalPublisher(Node):
-
+class ImageSubscriber(Node):
     def __init__(self):
-        super().__init__('minimal_publisher')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        super().__init__('image_subscriber')
+        self.bridge = CvBridge()
+        qos_profile = QoSProfile(reliability=ReliabilityPolicy.BEST_EFFORT, history=HistoryPolicy.KEEP_LAST, depth=10)
+        self.image_sub = self.create_subscription(CompressedImage, '/camera/color/image_raw/compressed', self.callback, qos_profile)
 
-    def timer_callback(self):
-        msg = String()
-        msg.data = 'Hello World: %d' % self.i
-        self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
-        self.i += 1
+    def callback(self, msg):
+        try:
+            # Dekodowanie skompresowanego obrazu
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            cv_image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
+            # Rozpoznawanie markerów ArUco
+            aruco_dict = cv2.aruco.Dictionary_get(cv2.aruco.DICT_6X6_250)
+            aruco_params = cv2.aruco.DetectorParameters_create()
+            corners, ids, rejected = cv2.aruco.detectMarkers(cv_image, aruco_dict, parameters=aruco_params)
+
+            # Rysowanie znalezionych markerów na obrazie
+            cv_image = cv2.aruco.drawDetectedMarkers(cv_image, corners, ids)
+
+            cv2.imshow("Image Window", cv_image)
+            cv2.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f"Error processing image: {e}")
 
 def main(args=None):
     rclpy.init(args=args)
-
-    minimal_publisher = MinimalPublisher()
-
-    rclpy.spin(minimal_publisher)
-
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    minimal_publisher.destroy_node()
-    rclpy.shutdown()
-
+    image_subscriber = ImageSubscriber()
+    try:
+        rclpy.spin(image_subscriber)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        image_subscriber.destroy_node()
+        rclpy.shutdown()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
