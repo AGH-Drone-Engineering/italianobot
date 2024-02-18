501f5af3073d0ab9f3e724ab45e6c6084054ac16
Carbon225 <carbon225@protonmail.com>
2024-02-18 01:27:14 +0100

    screen

--- itabot/launch/slam.launch.py
+++ itabot/launch/slam.launch.py
@@ -27,6 +27,7 @@ def generate_launch_description():
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
         name='slam',
+        output='screen',
         parameters=[slam_params_file, {'use_sim_time': use_sim_time}],
     )
 
