f8eaf2cab4af7bb5f95206caac1bf9f83c0a0db2
theSPGN <spgn@gmail.com>
2024-02-22 01:04:29 +0100

    test node

---
+++ itabot_aruco/itabot_aruco/distance_sim.py
@@ -0,0 +1,175 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
+from cv_bridge import CvBridge, CvBridgeError
+import cv2
+from cv2 import aruco
+import numpy as np
+from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
+from std_msgs.msg import String
+from geometry_msgs.msg import PoseWithCovarianceStamped  # PoseStamped ??????????
+
+
+class ArucoDetector(Node):
+
+    def __init__(self):
+
+        super().__init__("aruco_detector")
+        self.bridge = CvBridge()
+
+        # Camera subscribers:
+
+        qos_profile = QoSProfile(
+            reliability=ReliabilityPolicy.BEST_EFFORT,
+            history=HistoryPolicy.KEEP_LAST,
+            depth=10,
+        )
+
+        self.image_sub = self.create_subscription(
+            Image,
+            "/camera/color/image_raw",
+            self.img_callback,
+            qos_profile,
+        )
+
+        # !!! QOS PROBLEM:
+        self.calib_sub = self.create_subscription(
+            CameraInfo,
+            "/camera/color/camera_info",
+            self.calib_callback,
+            qos_profile,
+        )
+
+        # Aruco position publishers:
+
+        self.distance_pub = self.create_publisher(String, "/distance", 10)
+        self.position_pub = self.create_publisher(
+            PoseWithCovarianceStamped, "/aruco/pose", 10
+        )
+
+        # Aruco code parameters:
+
+        self.MARKER_SIZE = 10  # centimeters
+        self.marker_dict = aruco.Dictionary_get(aruco.DICT_ARUCO_ORIGINAL)
+        self.param_markers = aruco.DetectorParameters_create()
+
+        # Camera calibration from file:
+
+        """ # self.calib_data_path = "../calib_data/MultiMatrix.npz"
+        self.calib_data_path = (
+            "/home/husarion/ros2_ws/src/itabot_aruco/calib_data/MultiMatrix.npz"
+        )
+        self.calib_data = np.load(self.calib_data_path)
+        self.cam_mat = self.calib_data["camMatrix"]
+        self.dist_coef = self.calib_data["distCoef"]
+        # unnecessary:
+        # self.r_vectors = self.calib_data["rVector"]
+        # self.t_vectors = self.calib_data["tVector"] """
+
+    def calib_callback(self, msg):
+        # Camera calibration from topic:
+
+        self.cam_mat = np.array(msg.k).reshape((3, 3))
+        self.dist_coef = np.array(msg.d)
+
+    def img_callback(self, msg):
+        try:
+            # Decode the compressed image
+            np_arr = np.frombuffer(msg.data, np.uint8)
+            frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            marker_corners, marker_IDs, reject = aruco.detectMarkers(
+                gray_frame, self.marker_dict, parameters=self.param_markers
+            )
+            if marker_corners:
+                rVec, tVec, _ = aruco.estimatePoseSingleMarkers(
+                    marker_corners, self.MARKER_SIZE, self.cam_mat, self.dist_coef
+                )
+                total_markers = range(0, marker_IDs.size)
+                for ids, corners, i in zip(marker_IDs, marker_corners, total_markers):
+                    cv2.polylines(
+                        frame,
+                        [corners.astype(np.int32)],
+                        True,
+                        (0, 255, 255),
+                        4,
+                        cv2.LINE_AA,
+                    )
+                    corners = corners.reshape(4, 2)
+                    corners = corners.astype(int)
+                    top_right = corners[0].ravel()
+                    top_left = corners[1].ravel()
+                    bottom_right = corners[2].ravel()
+                    bottom_left = corners[3].ravel()
+
+                    distance = np.sqrt(
+                        tVec[i][0][2] ** 2 + tVec[i][0][0] ** 2 + tVec[i][0][1] ** 2
+                    )
+                    point = cv2.drawFrameAxes(
+                        frame, self.cam_mat, self.dist_coef, rVec[i], tVec[i], 4, 4
+                    )
+                    cv2.putText(
+                        frame,
+                        f"id: {ids[0]} Dist: {round(distance, 2)}",
+                        tuple(top_right),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1.3,
+                        (0, 0, 255),
+                        2,
+                        cv2.LINE_AA,
+                    )
+                    cv2.putText(
+                        frame,
+                        f"x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)} ",
+                        tuple(bottom_right),
+                        cv2.FONT_HERSHEY_PLAIN,
+                        1.0,
+                        (0, 0, 255),
+                        2,
+                        cv2.LINE_AA,
+                    )
+                    try:
+                        message = String()
+                        message.data = f"id: {ids[0]} Dist: {round(distance, 2)} x:{round(tVec[i][0][0],1)} y: {round(tVec[i][0][1],1)}"
+                        self.distance_pub.publish(message)
+
+                        aruco_position = (
+                            PoseWithCovarianceStamped()
+                        )  # PoseStamped() ?????????
+                        aruco_position.header.stamp = self.get_clock().now().to_msg()
+                        aruco_position.header.frame_id = (
+                            "map"  # "camera_orbbec_astra_link" ???????????
+                        )
+
+                        aruco_position.pose.pose.position.x = distance
+                        aruco_position.pose.pose.position.y = -round(tVec[i][0][0], 1)
+                        aruco_position.pose.pose.position.z = -round(tVec[i][0][1], 1)
+                        aruco_position.pose.pose.orientation.w = 1.0
+                        aruco_position.pose.pose.orientation.x = 0.0
+                        aruco_position.pose.pose.orientation.y = 0.0
+                        aruco_position.pose.pose.orientation.z = 0.0
+                        self.position_pub.publish(aruco_position)
+
+                    except Exception as e:
+                        self.get_logger().info(f"Publisher error: {e}")
+
+            cv2.imshow("frame", frame)
+            key = cv2.waitKey(1)
+            if key == ord("q"):
+                cv2.destroyAllWindows()
+        except CvBridgeError as e:
+            self.get_logger().error("Could not convert image: %s" % e)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    aruco_detector = ArucoDetector()
+    rclpy.spin(aruco_detector)
+    aruco_detector.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
--- itabot_aruco/setup.py
+++ itabot_aruco/setup.py
@@ -1,28 +1,28 @@
 from setuptools import find_packages, setup
 
-package_name = 'itabot_aruco'
+package_name = "itabot_aruco"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='carbon',
-    maintainer_email='carbon225@protonmail.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="carbon",
+    maintainer_email="carbon225@protonmail.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'detector = itabot_aruco.detector:main',
-	    'camera_capture = itabot_aruco.capture_calibration_images_ros2:main',
-	    'distance = itabot_aruco.distance:main',
+        "console_scripts": [
+            "detector = itabot_aruco.detector:main",
+            "camera_capture = itabot_aruco.capture_calibration_images_ros2:main",
+            "distance = itabot_aruco.distance:main",
+            "distance_sim = itabot_aruco.distance_sim:main",
         ],
     },
 )
