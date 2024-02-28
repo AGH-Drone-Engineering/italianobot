d8434bad13fe47dc8c6ef16762a7f0451f8a8fe3
theSPGN <spgn@gmail.com>
2024-02-28 14:57:51 +0100

    do sprawdzenia: najlepiej zamienic teraz te pkt na wsp na mapie zeby wyswietlic pkt na pliku pgm jako czerwone pixele

--- itabot_aruco/itabot_aruco/pgm_to_nav2points.py
+++ itabot_aruco/itabot_aruco/pgm_to_nav2points.py
@@ -3,6 +3,8 @@ from PIL import Image
 import yaml
 import os
 
+MARGIN = 10
+
 
 def load_map_data(yaml_file):
     with open(yaml_file, "r") as file:
@@ -13,12 +15,22 @@ def load_map_data(yaml_file):
 def calculate_goal_points(map_file, resolution, init_pose):
     img = Image.open(map_file)
     img_array = np.array(img)
-    available_points = np.where(img_array == 205)
     goal_points = []
-    for y, x in zip(*available_points):
-        real_x = x * resolution + init_pose[0]
-        real_y = y * resolution + init_pose[1]
-        goal_points.append((real_x, real_y))
+    height, width = img_array.shape
+    for y in range(1, height - 1):
+        for x in range(1, width - 1):
+            if np.all(img_array[y - 1 : y + 2, x - 1 : x + 2] == 205):
+                # Sprawdź, czy poprzedni punkt celu jest co najmniej MARGIN kratki dalej
+                if (
+                    len(goal_points) == 0
+                    or np.abs(goal_points[-1][0] - x * resolution - init_pose[0])
+                    > MARGIN * resolution
+                    or np.abs(goal_points[-1][1] - y * resolution - init_pose[1])
+                    > MARGIN * resolution
+                ):
+                    real_x = x * resolution + init_pose[0]
+                    real_y = y * resolution + init_pose[1]
+                    goal_points.append((real_x, real_y))
 
     return goal_points
 
@@ -34,3 +46,4 @@ map_data = load_map_data(yaml_file)
 resolution = map_data["resolution"]
 init_pose = map_data["origin"][:2]
 goal_points = calculate_goal_points(map_file, resolution, init_pose)
+print(len(goal_points))
