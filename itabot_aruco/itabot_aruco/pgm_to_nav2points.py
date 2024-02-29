dc27a5c889262a1d3fd4030a9e9cbae9c7945bbc
theSPGN <spgn@gmail.com>
2024-02-29 01:53:23 +0100

    Nav2 points from pgm -> return table of goal points to minimal_publisher -> from x points make 4 * x (w orientation in 4 dim) (rotation 360) -> comment lines for image saving in pgm_to_nav... -> z = 0

--- itabot_aruco/itabot_aruco/pgm_to_nav2points.py
+++ itabot_aruco/itabot_aruco/pgm_to_nav2points.py
@@ -3,7 +3,8 @@ from PIL import Image
 import yaml
 import os
 
-MARGIN = 10
+MARGIN = 10  # minimal spacing between points
+WALL_DET = 5  # minimal spacing between rosbot and wall
 
 
 def load_map_data(yaml_file):
@@ -15,22 +16,36 @@ def load_map_data(yaml_file):
 def calculate_goal_points(map_file, resolution, init_pose):
     img = Image.open(map_file)
     img_array = np.array(img)
+    out_put_img_array = (
+        img_array.copy()
+    )  # matrix to prepare visualization of the goal points
     goal_points = []
     height, width = img_array.shape
     for y in range(1, height - 1):
         for x in range(1, width - 1):
-            if np.all(img_array[y - 1 : y + 2, x - 1 : x + 2] == 205):
-                # Sprawdź, czy poprzedni punkt celu jest co najmniej MARGIN kratki dalej
-                if (
-                    len(goal_points) == 0
-                    or np.abs(goal_points[-1][0] - x * resolution - init_pose[0])
+            if np.all(
+                img_array[y - WALL_DET : y + WALL_DET, x - WALL_DET : x + WALL_DET]
+                == 254
+            ):
+                # free space between points:
+                if len(goal_points) == 0 or all(
+                    np.abs(gp[0] - x * resolution - init_pose[0]) > MARGIN * resolution
+                    or np.abs(gp[1] - y * resolution - init_pose[1])
                     > MARGIN * resolution
-                    or np.abs(goal_points[-1][1] - y * resolution - init_pose[1])
-                    > MARGIN * resolution
+                    for gp in goal_points[-width // MARGIN :]
                 ):
+
                     real_x = x * resolution + init_pose[0]
                     real_y = y * resolution + init_pose[1]
                     goal_points.append((real_x, real_y))
+
+                    # Saving map with goal points
+                    # Only for testing the algorithm
+                    out_put_img_array[y][x] = 50
+                    im = Image.fromarray(out_put_img_array)
+                    directory = os.path.dirname(map_file)
+                    points_file = os.path.join(directory, "points.pgm")
+                    im.save(points_file)
 
     return goal_points
 
@@ -46,4 +61,3 @@ map_data = load_map_data(yaml_file)
 resolution = map_data["resolution"]
 init_pose = map_data["origin"][:2]
 goal_points = calculate_goal_points(map_file, resolution, init_pose)
-print(len(goal_points))
