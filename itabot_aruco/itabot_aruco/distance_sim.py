6dc66edf11f9c6246114d833b283780cdb7200ab
theSPGN <spgn1226@gmail.com>
2024-03-05 02:15:26 +0100

    TRANSLACJE DO POPRAWY ASAP
    Signed-off-by: theSPGN <spgn1226@gmail.com>

--- itabot_aruco/itabot_aruco/distance_sim.py
+++ itabot_aruco/itabot_aruco/distance_sim.py
@@ -8,12 +8,12 @@ from cv2 import aruco
 import numpy as np
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
 from std_msgs.msg import String
-from geometry_msgs.msg import PoseWithCovarianceStamped
 from geometry_msgs.msg import TransformStamped
 import tf_transformations as tf_trans
 import tf2_ros
 import time
 import os
+from tf_transformations import quaternion_multiply
 
 
 class ArucoDetector(Node):
@@ -55,6 +55,11 @@ class ArucoDetector(Node):
 
         # tf broadcaster:
         self.aruco_broadcaster = tf2_ros.TransformBroadcaster(self)
+        self.arucos_found = dict()
+
+        # tf buffer and listener:
+        self.tf_buffer = tf2_ros.Buffer()
+        self.tf_listener = tf2_ros.TransformListener(self.tf_buffer, self)
 
         # do testow awaryjnie (potem do usuniecia):
         self.distance_pub = self.create_publisher(String, "/distance", 10)
@@ -99,8 +104,14 @@ class ArucoDetector(Node):
         rVec[i] = rVec_rotated.T
 
     def img_callback(self, msg):
-        try:
 
+        try:
+            for aruco_ in self.arucos_found.values():
+                self.aruco_broadcaster.sendTransform(aruco_)
+
+        except Exception as e:
+            self.get_logger().info(f"TF publish error {e}")
+        try:
             frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding="bgr8")
             frame2 = frame.copy()
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -181,12 +192,6 @@ class ArucoDetector(Node):
                         # Convert rotation matrix to quaternion
                         quaternion = tf_trans.quaternion_from_matrix(rVec_matrix)
 
-                        # PoseWithCovarianceStamped:
-                        if ids[0] not in self.aruco_publishers:
-                            self.aruco_publishers[ids[0]] = self.create_publisher(
-                                PoseWithCovarianceStamped, f"/aruco/pose/nr{ids[0]}", 10
-                            )
-
                         j = self.pictures_counter.get(ids[0], 0)
                         if j < 5:
                             self.pictures_counter[ids[0]] = j + 1
@@ -204,38 +209,64 @@ class ArucoDetector(Node):
                             except Exception as e:
                                 self.get_logger().info(f"{e}")
 
-                        aruco_position = PoseWithCovarianceStamped()
-                        aruco_position.header.stamp = self.get_clock().now().to_msg()
-                        aruco_position.header.frame_id = "camera_color_frame"
-                        self.position_pub = self.aruco_publishers[ids[0]]
-
-                        aruco_position.pose.pose.position.x = tVec[i][0][2]
-                        aruco_position.pose.pose.position.y = -tVec[i][0][0]
-                        aruco_position.pose.pose.position.z = -tVec[i][0][1]
-
-                        aruco_position.pose.pose.orientation.w = quaternion[0]
-                        aruco_position.pose.pose.orientation.x = quaternion[1]
-                        aruco_position.pose.pose.orientation.y = quaternion[2]
-                        aruco_position.pose.pose.orientation.z = quaternion[3]
-
-                        self.position_pub.publish(aruco_position)
-
                         # TF_broadcast:
+                        camera_to_base_link = self.tf_buffer.lookup_transform(
+                            "base_link", "camera_color_frame", rclpy.time.Time()
+                        )
+                        base_link_to_map = self.tf_buffer.lookup_transform(
+                            "map", "base_link", rclpy.time.Time()
+                        )
+
                         aruco_ekf = TransformStamped()
                         aruco_ekf.header.stamp = self.get_clock().now().to_msg()
-                        aruco_ekf.header.frame_id = "camera_color_frame"
+
+                        aruco_ekf.header.frame_id = "map"
                         aruco_ekf.child_frame_id = f"aruco_marker_{ids[0]}"
 
-                        aruco_ekf.transform.translation.x = tVec[i][0][2]
-                        aruco_ekf.transform.translation.y = -tVec[i][0][0]
-                        aruco_ekf.transform.translation.z = -tVec[i][0][1]
+                        aruco_ekf.transform.translation.x = (
+                            tVec[i][0][2]
+                            + camera_to_base_link.transform.translation.x
+                            + base_link_to_map.transform.translation.x
+                        )
+                        aruco_ekf.transform.translation.y = (
+                            -tVec[i][0][0]
+                            + camera_to_base_link.transform.translation.y
+                            + base_link_to_map.transform.translation.y
+                        )
+                        aruco_ekf.transform.translation.z = (
+                            -tVec[i][0][1]
+                            + camera_to_base_link.transform.translation.z
+                            + base_link_to_map.transform.translation.z
+                        )
 
-                        aruco_ekf.transform.rotation.w = quaternion[0]
-                        aruco_ekf.transform.rotation.x = quaternion[1]
-                        aruco_ekf.transform.rotation.y = quaternion[2]
-                        aruco_ekf.transform.rotation.z = quaternion[3]
+                        # Convert the rotation from the camera to the base_link to a quaternion
+                        camera_to_base_link_quaternion = [
+                            camera_to_base_link.transform.rotation.x,
+                            camera_to_base_link.transform.rotation.y,
+                            camera_to_base_link.transform.rotation.z,
+                            camera_to_base_link.transform.rotation.w,
+                        ]
+                        base_link_to_map_quaternion = [
+                            base_link_to_map.transform.rotation.x,
+                            base_link_to_map.transform.rotation.y,
+                            base_link_to_map.transform.rotation.z,
+                            base_link_to_map.transform.rotation.w,
+                        ]
 
-                        self.aruco_broadcaster.sendTransform(aruco_ekf)
+                        # Multiply the quaternions to combine the rotations
+                        combined_quaternion_base_link = quaternion_multiply(
+                            quaternion, camera_to_base_link_quaternion
+                        )
+                        combined_quaternion = quaternion_multiply(
+                            combined_quaternion_base_link, base_link_to_map_quaternion
+                        )
+
+                        aruco_ekf.transform.rotation.w = combined_quaternion[0]
+                        aruco_ekf.transform.rotation.x = combined_quaternion[1]
+                        aruco_ekf.transform.rotation.y = combined_quaternion[2]
+                        aruco_ekf.transform.rotation.z = combined_quaternion[3]
+
+                        self.arucos_found[ids[0]] = aruco_ekf
 
                     except Exception as e:
                         self.get_logger().info(f"muj Publisher error: {e}")
