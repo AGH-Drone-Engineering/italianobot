47c604a59061a1597d95ee18356eaae0ee4595ef
theSPGN <spgn1226@gmail.com>
2024-03-07 16:30:28 +0100

    Fixing bugs with odom topic and removing unnecessary files

--- itabot_nav/launch/bringup.launch.py
+++ itabot_nav/launch/bringup.launch.py
@@ -10,8 +10,12 @@ from launch.actions import (
     SetEnvironmentVariable,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch.substitutions import (
+    EnvironmentVariable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.actions import Node, SetParameter
 from launch_ros.descriptions import ParameterFile
 from nav2_common.launch import RewrittenYaml
 from launch.actions import TimerAction
@@ -152,10 +156,93 @@ def generate_launch_description():
 
     # Create the launch description and populate
 
+    namespace = LaunchConfiguration("namespace")
+    declare_namespace_arg = DeclareLaunchArgument(
+        "namespace",
+        default_value=EnvironmentVariable("ROBOT_NAMESPACE", default_value=""),
+        description="Namespace for all topics and tfs",
+    )
+
+    use_sim = LaunchConfiguration("use_sim")
+    declare_use_sim_arg = DeclareLaunchArgument(
+        "use_sim",
+        default_value="False",
+        description="Whether simulation is used",
+    )
+
+    use_gpu = LaunchConfiguration("use_gpu")
+    declare_use_gpu_arg = DeclareLaunchArgument(
+        "use_gpu",
+        default_value="False",
+        description="Whether GPU acceleration is used",
+    )
+
+    simulation_engine = LaunchConfiguration("simulation_engine")
+    declare_simulation_engine_arg = DeclareLaunchArgument(
+        "simulation_engine",
+        default_value="webots",
+        description="Which simulation engine to be used",
+        choices=["ignition-gazebo", "gazebo-classic", "webots"],
+    )
+
+    rosbot_controller = get_package_share_directory("rosbot_controller")
+    rosbot_bringup = get_package_share_directory("rosbot_bringup")
+
+    mecanum = LaunchConfiguration("mecanum")
+    declare_mecanum_arg = DeclareLaunchArgument(
+        "mecanum",
+        default_value="False",
+        description=(
+            "Whether to use mecanum drive controller (otherwise diff drive controller is used)"
+        ),
+    )
+
+    controller_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution(
+                [
+                    rosbot_controller,
+                    "launch",
+                    "controller.launch.py",
+                ]
+            )
+        ),
+        launch_arguments={
+            "use_sim": use_sim,
+            "mecanum": mecanum,
+            "use_gpu": use_gpu,
+            "simulation_engine": simulation_engine,
+            "namespace": namespace,
+        }.items(),
+    )
+
+    ekf_config = PathJoinSubstitution([rosbot_bringup, "config", "ekf.yaml"])
+
+    robot_localization_node = Node(
+        package="robot_localization",
+        executable="ekf_node",
+        output="screen",
+        parameters=[ekf_config],
+        remappings=[
+            ("/diagnostics", "diagnostics"),
+            ("/tf", "tf"),
+            ("/tf_static", "tf_static"),
+        ],
+        namespace=namespace,
+    )
+
     ld = LaunchDescription()
 
     ld.add_action(stdout_linebuf_envvar)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(bringup_cmd_group)
+    ld.add_action(declare_namespace_arg)
+    ld.add_action(declare_mecanum_arg)
+    ld.add_action(declare_use_sim_arg)
+    ld.add_action(declare_use_gpu_arg)
+    ld.add_action(declare_simulation_engine_arg)
+    ld.add_action(SetParameter(name="use_sim_time", value=use_sim))
+    ld.add_action(controller_launch)
+    ld.add_action(robot_localization_node)
 
     return ld
