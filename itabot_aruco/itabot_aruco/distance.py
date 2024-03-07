0942563b43f39cba06fe026a9cafe37ef484065b
theSPGN <spgn1226@gmail.com>
2024-03-07 15:21:30 +0100

    .
    Signed-off-by: theSPGN <spgn1226@gmail.com>

--- itabot_aruco/itabot_aruco/distance.py
+++ itabot_aruco/itabot_aruco/distance.py
@@ -313,10 +313,17 @@ class ArucoDetector(Node):
                             try:
 
                                 home_dir = os.environ["HOME"]
-                                image_file = os.path.join(
+                                image_dir = os.path.join(
                                     home_dir,
-                                    f"ros2_ws/src/italianobot/itabot_aruco/itabot_aruco/pictures/Aruco{ids[0]}_photo_nr_{j}.jpg",
+                                    "ros2_ws/src/italianobot/itabot_aruco/itabot_aruco/raports",
                                 )
+                                os.makedirs(
+                                    image_dir, exist_ok=True
+                                )  # Create directory if it does not exist
+                                image_file = os.path.join(
+                                    image_dir, f"Aruco{ids[0]}_photo_nr_{j}.jpg"
+                                )
+
                                 cv2.imwrite(image_file, frame2)
 
                                 if ids[0] in self.arucos_found_cnt10:
