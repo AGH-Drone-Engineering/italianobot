b822e8d7388cd7b609ae67d2e347da3e61da1859
DK333D <daria39@onet.com.pl>
2024-03-10 13:24:43 +0100

    pc_start.sh start italianobot
    Signed-off-by: DK333D <daria39@onet.com.pl>

--- pc_start.sh
+++ pc_start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# sshpass or -i ~/.ssh/id_rsa
+export IP=192.168.1.106
+
+sshpass -p 'husarion' ssh -i ~/.ssh/id_rsa husarion@$IP '. ~/ros2_ws/src/italianobot/italiano_start.sh'
+
 ros2 launch itabot real.launch
